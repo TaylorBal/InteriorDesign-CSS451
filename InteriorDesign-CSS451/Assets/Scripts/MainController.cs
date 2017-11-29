d9ae0715a52ef57091d1829ee21076aae79dbf56
Nick Carpenetti <nickcarpenetti@gmail.com>
2017-11-28 18:51:01 -0800

    Added some basic constructs to the project (although they are mostly empty)

---
+++ InteriorDesign-CSS451/Assets/Scripts/MainController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class MainController : MonoBehaviour {
+
+    public TheWorld theWorld = null;        //represents "The Model" in our design
+
+    //Camera
+    public CameraControl mainCamera = null;
+
+    //EventSystem For Input
+    publi
+
+    //UI Elements Go Here
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Assert(theWorld != null);
+        Debug.Assert(mainCamera != null);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ProcessInput();
+    }
+
+
+}
