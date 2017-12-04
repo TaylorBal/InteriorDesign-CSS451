cab884acfce1683a8f467baf681e56afd8c08796
TaylorBal <ebaldwin742@gmail.com>
2017-12-03 16:45:10 -0800

    Preview camera

---
+++ InteriorDesign-CSS451/Assets/Scripts/PreviewCameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewCameraControl : MonoBehaviour {
+
+    GameObject previewObject = null;
+    Vector3 offset = new Vector3(0, -1, -4);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SnapObjectToCamera(GameObject previewObject)
+    {
+        previewObject.transform.localPosition = transform.localPosition + offset;
+    }
+}
