8a2b6061a86c280d21fe041809745ea2493e9ea7
Nick Carpenetti <nickcarpenetti@gmail.com>
2017-12-11 20:54:12 -0800

    made changes to the way selection works

---
+++ InteriorDesign-CSS451/Assets/SelectionDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionDetector : MonoBehaviour {
+
+    public Furniture parent = null;
+    public BoxCollider detectionCollider;
+    public Vector3 detectorOffset = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Assert(parent != null);
+
+        detectionCollider = GetComponent<BoxCollider>();
+	}
+
+    public void SetCenter(Vector3 center)
+    {
+        detectionCollider.center = center + detectorOffset;
+    }
+
+    public void SetSize(Vector3 size)
+    {
+        detectionCollider.size = size;
+    }
+
+    public Furniture GetParent()
+    {
+        return parent;
+    }
+}
